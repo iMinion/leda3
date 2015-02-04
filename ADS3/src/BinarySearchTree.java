d65b563590a6e9a05add8f516162118c4fca4fd1
iMinion <k.srirammanoj@gmail.com>
2015-02-04 19:06:31 +0530

    BST Insertion Stage 1

--- ADS3/src/BinarySearchTree.java
+++ ADS3/src/BinarySearchTree.java
@@ -1,5 +1,22 @@
-
 public class BinarySearchTree {
-
+	private Node root = new Node();
 	
+	public void insert(int data) {
+		insert(root, data);
+	}
+	
+	public void insert(Node node, int data) {
+		if(node == null) {
+			node = new Node(data);
+		}
+		else if(node.getData() > data) {
+			insert(node.getLeft(), data);
+		}
+		else {
+			insert(node.getRight(), data);
+		}
+	}
+	public static void main(String[] args) {
+		
+	}
 }
