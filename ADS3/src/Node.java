962f5077801f039248947c9be3cb9c5c9a709dae
iMinion <k.srirammanoj@gmail.com>
2015-02-04 18:19:17 +0530

    The initial commit

---
+++ ADS3/src/Node.java
@@ -0,0 +1,21 @@
+
+public class Node {
+	int data;
+	Node left = null;
+	Node right = null;
+	
+	public Node(int data) {
+		this.data = data;
+	}
+	
+	public void setLeft(Node l) {
+		left = l;
+	}
+	
+	public void setRight(Node r) {
+		right = r;
+	}
+	
+	
+	
+}
