b3cd0b2d44cad85b18ef1b84e634f4a1d7949c28
iMinion <k.srirammanoj@gmail.com>
2015-02-05 19:15:28 +0530

    Working insertion of a bst

--- ADS3/src/BinarySearchTree.java
+++ ADS3/src/BinarySearchTree.java
@@ -1,49 +1,47 @@
-import java.util.Stack;
-
 public class BinarySearchTree {
-	private Node root;
+	private Node root = null;
+	private Node node; 
 	private StringBuilder sb = new StringBuilder();
 	public void insert(int data) {
-		insert(root, data);
+		if(root == null) {
+			root = new Node(data);
+			node = root;
+		}
+		else insert(node, new Node(data));
 	}
 	
-	public void insert(Node node, int data) {
-		if(node == null) {
-			node = new Node(data);
+	public void insert(Node root, Node data) {
+		if(root == null) {
+			root = data;
 		}
-		else if(node.getData() > data) {
-			insert(node.getLeft(), data);
+		else if(root.getData() > data.getData()) {
+			if(root.getLeft() == null) {
+				root.setLeft(data); 
+			}
+			else insert(root.getLeft(), data);
 		}
 		else {
-			insert(node.getRight(), data);
+			if(root.getRight() == null) {
+				root.setRight(data);
+			}
+			else insert(root.getRight(), data);
 		}
 	}
 	
-	Stack<Node> myStack = new Stack<Node>();
 	public void inOrder() {
 		inOrder(root);
 	}
 	
 	public void inOrder(Node root) {
-		myStack.push(root);
-		if(root == null) {
-			if(myStack.size() == 1) {
-				myStack.pop();
-				return;
-			}
-			else {
-				sb.append(myStack.pop());
-				inOrder(myStack.peek().getRight());
-			}
-		}
-		else {
+		if(root != null) {
 			inOrder(root.getLeft());
+			System.out.println(root.getData());
+			inOrder(root.getRight());
 		}
 	}
 	
 	public String toString() {
-		
-		return null;
+		return sb.toString();
 	}
 	
 	public static void main(String[] args) {
@@ -51,5 +49,7 @@ public class BinarySearchTree {
 		for(int i = 0; i < 10; ++i) {
 			bst.insert(i);
 		}
+		bst.inOrder();
+		System.out.println(bst);
 	}
 }
