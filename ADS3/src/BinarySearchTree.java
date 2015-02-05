0ec694328b4c1de99b68c25fa8d8d5d1cfc81713
iMinion <k.srirammanoj@gmail.com>
2015-02-05 20:24:52 +0530

    Search works fine for elements in the list

--- ADS3/src/BinarySearchTree.java
+++ ADS3/src/BinarySearchTree.java
@@ -1,13 +1,19 @@
+import java.util.Scanner;
+
 public class BinarySearchTree {
 	private Node root = null;
-	private Node node; 
 	private StringBuilder sb = new StringBuilder();
+	private int max;
+	private int count;
 	public void insert(int data) {
 		if(root == null) {
 			root = new Node(data);
-			node = root;
+			max = data;
 		}
-		else insert(node, new Node(data));
+		else {
+			if(data > max) max = data;
+			insert(root, new Node(data));
+		}
 	}
 	
 	public void insert(Node root, Node data) {
@@ -28,15 +34,64 @@ public class BinarySearchTree {
 		}
 	}
 	
-	public void inOrder() {
+	public String inOrder() {
+		sb = new StringBuilder();
 		inOrder(root);
+		if(sb.length() > 0) return sb.toString().substring(0, sb.length() -1);
+		else return sb.toString();
 	}
 	
 	public void inOrder(Node root) {
 		if(root != null) {
 			inOrder(root.getLeft());
-			System.out.println(root.getData());
+			sb.append(Integer.toString(root.getData()));
+			sb.append(" ");
 			inOrder(root.getRight());
+		}
+	}
+	
+	public String postOrder() {
+		sb = new StringBuilder();
+		postOrder(root);
+		if(sb.length() > 0) return sb.toString().substring(0, sb.length() -1);
+		else return sb.toString();
+	}
+	
+	public void postOrder(Node root) {
+		if(root != null) {
+			postOrder(root.getLeft());
+			postOrder(root.getRight());
+			sb.append(Integer.toString(root.getData()));
+			sb.append(" ");
+		}
+	}
+	
+	public String search(int data) {
+		sb = new StringBuilder();
+		count = 0;
+		sb.append(search(root, new Node(data), null));
+		sb.append(", ");
+		sb.append(count);
+		return sb.toString();
+	}
+	
+	public boolean search(Node root, Node node, Node prev) {
+		if(root == null || node == null) {
+			if(prev == null) count = 0;
+			else count = prev.getData();
+			return false;
+		}
+		else {
+			count++;
+			if(root.getData() > node.getData()) {
+				return search(root.getLeft(), node, root);
+			}
+			else if(root.getData() < node.getData()) {
+				return search(root.getRight(), node, root);
+			}
+			else {
+				return true;
+			}
 		}
 	}
 	
@@ -46,10 +101,13 @@ public class BinarySearchTree {
 	
 	public static void main(String[] args) {
 		BinarySearchTree bst = new BinarySearchTree();
-		for(int i = 0; i < 10; ++i) {
-			bst.insert(i);
+		Scanner sc = new Scanner(System.in);
+		String str[] = sc.nextLine().split(", ");
+		for(int i = 0; i < str.length; ++i) {
+			bst.insert(Integer.parseInt(str[i]));
+			System.out.println(bst.inOrder());
 		}
-		bst.inOrder();
-		System.out.println(bst);
+		System.out.println(bst.search(2));
+		sc.close();
 	}
 }
